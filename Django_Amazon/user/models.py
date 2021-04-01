46c84ab2bbcc67a732f594625b7ee0c0409658ca
Samir <Saitwalsamir@gmail.com>
2021-04-01 20:07:03 +0530

    --user app added

---
+++ Django_Amazon/user/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+# Create your models here.
+class User(AbstractUser):
+    is_amazon_admin = models.BooleanField(default=False)
+    is_amazon_seller = models.BooleanField(default=False)
+    is_amazon_customer = models.BooleanField(default=False)
+    is_amazon_employee = models.BooleanField(default=False)
+    is_amazon_delivery_service = models.BooleanField(default=False)
