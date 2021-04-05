54ea7b2683efede4165db58189d35673075966af
Samir <Saitwalsamir@gmail.com>
2021-04-05 21:43:31 +0530

    --updated

--- Django_Amazon/amazon_admin/utils.py
+++ Django_Amazon/amazon_admin/utils.py
@@ -3,11 +3,9 @@ import uuid
 
 def Unique_Name():
     username = uuid.uuid4().hex[:6].upper()
-    print(username)
     return username
 
 
 def Unique_Password():
     password = uuid.uuid4().hex[:6].upper()
-    print(password)
     return password
