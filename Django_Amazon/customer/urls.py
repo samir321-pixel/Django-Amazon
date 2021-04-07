c5fbfbad40e9e81b6d5fc57e5c772558b578ab72
damini321 <daminikhule99@gmail.com>
2021-04-08 00:17:00 +0530

    --customer notification added

--- Django_Amazon/customer/urls.py
+++ Django_Amazon/customer/urls.py
@@ -4,4 +4,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('amazon_customer_signup/', Amazon_Customers_Signup_View.as_view()),
+    path('amazon_customer_notification/', Amazon_Customer_Notification_View.as_view()),
 ]
