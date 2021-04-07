0f8564c7f8d884e63208d41a4b5667776f82dc31
Samir <Saitwalsamir@gmail.com>
2021-04-07 21:26:36 +0530

    --admin list api added

--- Django_Amazon/amazon_admin/urls.py
+++ Django_Amazon/amazon_admin/urls.py
@@ -4,4 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('amazon_admin_signup/', Amazon_Admin_Signup_View.as_view()),
     path('amazon_admin_notification/', Amazon_Admin_Notification_View.as_view()),
+    path('amazon_admin_list/', Amazon_Admin_ListView.as_view()),
 ]
--- Django_Amazon/amazon_admin/views.py
+++ Django_Amazon/amazon_admin/views.py
@@ -47,4 +47,7 @@ class Amazon_Admin_ListView(generics.ListAPIView):
 
     def list(self, request, *args, **kwargs):
         if self.request.user.is_superuser:
-            serializer = self.get_serializer(queryset)
+            serializer = self.get_serializer(self.get_queryset(), many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response({"NO_ACCESS": "Access Denied"}, status=status.HTTP_401_UNAUTHORIZED)
