d8a4659b301144cf9863b53cb7054c6f5035ddda
damini321 <daminikhule99@gmail.com>
2021-04-20 19:36:33 +0530

    --Retrieve & Update active added

--- Django_Amazon/amazon_delivery_service/views.py
+++ Django_Amazon/amazon_delivery_service/views.py
@@ -212,28 +212,35 @@ class Manage_Amazon_Delivery_Boy_List_View(generics.ListAPIView):
 # Create Manage_Amazon_Delivery_Boy Update >> Active>> Notification >>Inactive Inactive notification
 
 class Manage_Amazon_Delivery_Boy_Retrieve_View(generics.RetrieveUpdateAPIView):
-    queryset = Amazon_Delivery_Boy.objects.all()
+    queryset = Amazon_Delivery_Service.objects.all()
     serializer_class = Manage_Amazon_Delivery_Boy_Update_View_Serializer
 
     def retrieve(self, request, *args, **kwargs):
         if self.request.user.is_amazon_delivery_service:
-            try:
-                query = Amazon_Delivery_Boy.objects.get(id=self.kwargs["id"])
-                serializer = self.get_serializer(query)
-                return Response(serializer.data, status=status.HTTP_200_OK)
-            except ObjectDoesNotExist:
-                return Response({"DOES_NOT_EXIST": "Does not exist"}, status=status.HTTP_404_NOT_FOUND)
+            amazon_delivery_service_query = Amazon_Delivery_Service.objects.get(user=self.request.user.id)
+            if amazon_delivery_service_query.active:
+                try:
+                    query = Amazon_Delivery_Boy.objects.get(id=self.kwargs["id"])
+                    serializer = self.get_serializer(query)
+                    return Response(serializer.data, status=status.HTTP_200_OK)
+                except ObjectDoesNotExist:
+                    return Response({"DOES_NOT_EXIST": "Does not exist"}, status=status.HTTP_404_NOT_FOUND)
+            else:
+                return Response({"NO_ACCESS": "Access Denied"}, status=status.HTTP_401_UNAUTHORIZED)
         else:
             return Response({"NO_ACCESS": "Access Denied"}, status=status.HTTP_401_UNAUTHORIZED)
 
     def update(self, request, *args, **kwargs):
         if self.request.user.is_amazon_delivery_service:
-            try:
-                query = Amazon_Delivery_Boy.objects.get(id=self.kwargs["id"])
-                serializer = self.get_serializer(query)
-                return Response({"DOES_NOT_EXIST": "Does not exist"}, status=status.HTTP_404_NOT_FOUND)
-            except ObjectDoesNotExist:
-                return Response(serializer.data, status=status.HTTP_200_OK)
-
+            amazon_delivery_service_query = Amazon_Delivery_Service.objects.get(user=self.request.user.id)
+            if amazon_delivery_service_query.active:
+                try:
+                    query = Amazon_Delivery_Boy.objects.get(id=self.kwargs["id"])
+                    serializer = self.get_serializer(query)
+                    return Response({"DOES_NOT_EXIST": "Does not exist"}, status=status.HTTP_404_NOT_FOUND)
+                except ObjectDoesNotExist:
+                    return Response(serializer.data, status=status.HTTP_200_OK)
+            else:
+                return Response({"NO_ACCESS": "Access Denied"}, status=status.HTTP_401_UNAUTHORIZED)
         else:
             return Response({"NO_ACCESS": "Access Denied"}, status=status.HTTP_401_UNAUTHORIZED)
