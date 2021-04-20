72b4fd8b2c9ceb735d350711d7e97c0728ff7800
Samir <Saitwalsamir@gmail.com>
2021-04-20 20:46:46 +0530

    Revert "--Update"

--- Django_Amazon/amazon_delivery_service/models.py
+++ Django_Amazon/amazon_delivery_service/models.py
@@ -18,22 +18,10 @@ id_proof = (
 
 # Create Amazon_Delivery_Boy model >> delivery boy will be under Amazon_Delivery_Service >>
 # Create delivery boy notifications
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
 # Create delivery service sign up >> for sign up create unique id of 5 digit and password of 5 digit already done
 # create a delivery boy service signup & 6 digit id+ 6 digit password
 # Create manage Amazon_Delivery_Service
 # Amazon_Delivery_Service will be under amazon superuser
-=======
-# Create delivery service sign up >> for sign up create unique id of 5 digit and password of 5 digit
-# Create manage Amazon_Delivery_Service
-# Amazon_Delivery_Service will be under amazon admin
->>>>>>> Stashed changes
-=======
-# Create delivery service sign up >> for sign up create unique id of 5 digit and password of 5 digit
-# Create manage Amazon_Delivery_Service
-# Amazon_Delivery_Service will be under amazon admin
->>>>>>> Stashed changes
 # create Manage Amazon_Delivery_Boy API
 # Amazon_Delivery_Boy will be under Amazon_Delivery_Service
 
@@ -42,8 +30,6 @@ class Amazon_Delivery_Service(models.Model):
     unique_id = models.CharField(max_length=200, unique=True, editable=False, null=True, blank=True)
     service_name = models.CharField(max_length=200, unique=True)
     establish_date = models.DateField()
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
     phone = PhoneField(blank=False, unique=True)
     alt_phone = PhoneField(blank=False)
     email = models.EmailField(unique=True)
@@ -118,116 +104,41 @@ class Amazon_Delivery_Boy(models.Model):
     alt_phone = PhoneField(blank=False)
     email = models.EmailField(unique=True)
     profile_photo = models.ImageField(upload_to="media/Amazon_Delivery_Boy/profile")
-=======
-    phone = PhoneField(blank=False, unique=True)
-    alt_phone = PhoneField(blank=False)
-    email = models.EmailField(unique=True)
-    profile_photo = models.ImageField(upload_to="media/Amazon_Delivery_Service/profile")
->>>>>>> Stashed changes
-=======
-    phone = PhoneField(blank=False, unique=True)
-    alt_phone = PhoneField(blank=False)
-    email = models.EmailField(unique=True)
-    profile_photo = models.ImageField(upload_to="media/Amazon_Delivery_Service/profile")
->>>>>>> Stashed changes
     active = models.BooleanField(default=False)
     Address = models.CharField(max_length=200)
     city = models.CharField(max_length=20)
     state = INStateField(null=True, blank=True)
     pincode = models.PositiveIntegerField(default=0)
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
     id_proof = models.CharField(max_length=30, choices=id_proof)
     id_proof_file = models.FileField(upload_to="media/Amazon_Delivery_Boy/id_proof_file")
-=======
-    certificate = models.FileField(upload_to="media/Amazon_Delivery_Service/certificate")
->>>>>>> Stashed changes
-=======
-    certificate = models.FileField(upload_to="media/Amazon_Delivery_Service/certificate")
->>>>>>> Stashed changes
     created_at = models.DateTimeField(auto_now=True)
     updated_at = models.DateTimeField(auto_now_add=True)
     get_notified = models.BooleanField(default=True)
     password = models.CharField(max_length=300, null=False, blank=False)
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
     Resume = models.FileField(upload_to="media/Amazon_Delivery_Boy/resume_file")
     qr_code = models.ImageField(upload_to='media/Amazon_Delivery_Boy/qr_codes', blank=True)
 
     # def __str__(self):
     #     return "{} {}".format("delivery boy", self.first_name)
-=======
-    qr_code = models.ImageField(upload_to='media/Amazon_Delivery_Service/qr_codes', blank=True)
->>>>>>> Stashed changes
-=======
-    qr_code = models.ImageField(upload_to='media/Amazon_Delivery_Service/qr_codes', blank=True)
->>>>>>> Stashed changes
 
     def __str__(self):
-        return "{}".format(self.service_name)
+        return "{}".format(self.first_name)
 
-
-<<<<<<< Updated upstream
 
 class Amazon_Delivery_Boy_Notifications(models.Model):
     amazon_delivery_boy = models.ForeignKey(Amazon_Delivery_Boy, on_delete=models.CASCADE, null=True,
                                             blank=True)
-=======
-class Amazon_Delivery_Service_Notifications(models.Model):
-    amazon_delivery_service = models.ForeignKey(Amazon_Delivery_Service, on_delete=models.CASCADE, null=True,
-                                                blank=True)
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
     message = models.TextField()
     seen = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now=True)
     updated_at = models.DateTimeField(auto_now_add=True)
 
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
     def register_delivery_boy(self, amazon_delivery_boy, amazon_delivery_boy_name, email, from_email):
         subject = "Register Successful"
         message = "Hi {} Thanks for registering. Your account is under reviewed we will get back to you soon!".format(
             amazon_delivery_boy_name)
         Amazon_Delivery_Boy_Notifications.objects.create(amazon_delivery_boy=amazon_delivery_boy,
                                                          message=message)
-=======
-=======
->>>>>>> Stashed changes
-    def register_delivery_service(self, amazon_delivery_service, service_name, email, from_email):
-        subject = "Register Successful"
-        message = "Hi {} Thanks for registering. Your account is under reviewed we will get back to you soon!".format(
-            service_name)
-        Amazon_Delivery_Service_Notifications.objects.create(amazon_delivery_service=amazon_delivery_service,
-                                                             message=message)
-        try:
-            send_mail(subject, message, from_email, [email])
-        except Exception as e:
-            print("Failed to send Mail", e)
-
-    def account_activated(self, amazon_delivery_service, service_name, email, from_email, unique_id, password):
-        subject = "Activated Successful"
-        message = "Hi {}, your account is successfully activated here is your unique id {} and password {}".format(
-            service_name, unique_id, password)
-        Amazon_Delivery_Service_Notifications.objects.create(amazon_delivery_service=amazon_delivery_service,
-                                                             message=message)
-        try:
-            send_mail(subject, message, from_email, [email])
-        except Exception as e:
-            print("Failed to send Mail", e)
-
-    def account_deactivated(self, amazon_delivery_service, service_name, email, from_email):
-        subject = "Accound Deactivated"
-        message = "Hi {}, your account is deactivated. Thank you for your service".format(
-            service_name)
-        Amazon_Delivery_Service_Notifications.objects.create(amazon_delivery_service=amazon_delivery_service,
-                                                             message=message)
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
         try:
             send_mail(subject, message, from_email, [email])
         except Exception as e:
