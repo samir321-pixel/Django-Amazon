8029edfcf27976289392b302f9de44d62d8cdfbc
damini321 <daminikhule99@gmail.com>
2021-04-23 01:33:00 +0530

    --Amazon proprietor notifications view added

--- Django_Amazon/amazon_proprietor/serializers.py
+++ Django_Amazon/amazon_proprietor/serializers.py
@@ -6,3 +6,9 @@ class Amazon_Proprietor_Signup_Serializer(serializers.ModelSerializer):
     class Meta:
         model = Amazon_Proprietor
         exclude = ["user", "unique_id", "qr_code", "get_notified", "password"]
+
+
+class Amazon_Proprietor_Notifications_Serializer(serializers.ModelSerializer):
+    class Meta:
+        model = Amazon_Proprietor_Notifications
+        fields = '__all__'
--- Django_Amazon/amazon_proprietor/views.py
+++ Django_Amazon/amazon_proprietor/views.py
@@ -49,3 +49,20 @@ class Amazon_Proprietor_Signup_View(generics.CreateAPIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status=status.HTTP_406_NOT_ACCEPTABLE)
+
+class Amazon_Proprietor_Notifications_View(generics.ListAPIView):
+    queryset = Amazon_Proprietor_Notifications.objects.all()
+    serializer_class = Amazon_Proprietor_Notifications_Serializer
+
+    def list(self, request, *args, **kwargs):
+        if self.request.user.is_amazon_admin:
+            amazon_proprietor_query = Amazon_Proprietor.objects.get(user=self.request.user)
+            if amazon_proprietor_query.active:
+                query = Amazon_Proprietor_Notifications.objects.get(
+                    amazon_proprietor=amazon_proprietor_query)
+                serializer = self.get_serializer(query, many=True)
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            else:
+                return Response({"NO_ACCESS": "Access Denied"}, status=status.HTTP_401_UNAUTHORIZED)
+        else:
+            return Response({"NO_ACCESS": "Access Denied"}, status=status.HTTP_401_UNAUTHORIZED)
