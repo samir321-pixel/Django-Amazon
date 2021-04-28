4e507868573e2e0636c1afdc38bb5e17da783eac
damini321 <daminikhule99@gmail.com>
2021-04-28 16:25:08 +0530

    --Seller profile view added

--- Django_Amazon/amazon_seller/urls.py
+++ Django_Amazon/amazon_seller/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('amazon_seller_notification/', Amazon_Seller_Notification_View.as_view()),
     path('manage_amazon_seller/', Manage_Amazon_Seller_List_View.as_view()),
     path('manage_amazon_seller/<int:id>/', Manage_Amazon_Seller_Retrieve_Update_View.as_view()),
+    path('amazon_admin_seller/', Amazon_Seller_Profile_View.as_view()),
 ]
--- Django_Amazon/amazon_seller/views.py
+++ Django_Amazon/amazon_seller/views.py
@@ -135,3 +135,36 @@ class Manage_Amazon_Seller_Retrieve_Update_View(generics.RetrieveUpdateAPIView):
                 return Response(serializer.errors, status=status.HTTP_406_NOT_ACCEPTABLE)
         else:
             return Response({"NO_ACCESS": "Access Denied"}, status=status.HTTP_401_UNAUTHORIZED)
+
+class Amazon_Seller_Profile_View(generics.RetrieveUpdateAPIView):
+    queryset = Amazon_Seller.objects.all()
+    serializer_class = Amazon_Seller_List_View_Serializer
+
+    @xframe_options_sameorigin
+    def retrieve(self, request, *args, **kwargs):
+        if self.request.user.is_amazon_seller:
+            print("Log in user id is", self.request.user.id)
+            user_query = User.objects.get(id=self.request.user.id)
+            print(user_query, "this is user query")
+            seller_query = Amazon_Seller.objects.get(user=user_query)
+            print(seller_query, "Admin")
+            serializer = self.get_serializer(seller_query)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response({"NO_ACCESS": "Access Denied"}, status=status.HTTP_401_UNAUTHORIZED)
+
+    @xframe_options_sameorigin
+    def update(self, request, *args, **kwargs):
+        if self.request.user.is_amazon_seller:
+            try:
+                instance = Amazon_Seller.objects.get(id=self.kwargs["id"])
+            except ObjectDoesNotExist:
+                return Response({"DOES_NOT_EXIST": "Does not exist"}, status=status.HTTP_404_NOT_FOUND)
+            serializer = self.get_serializer(instance, data=self.request.data, partial=True)
+            if serializer.is_valid(raise_exception=True):
+                serializer.save(updated_at=datetime.datetime.now(), active=True)
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            else:
+                return Response(serializer.errors, status=status.HTTP_406_NOT_ACCEPTABLE)
+        else:
+            return Response({"NO_ACCESS": "Access Denied"}, status=status.HTTP_401_UNAUTHORIZED)
