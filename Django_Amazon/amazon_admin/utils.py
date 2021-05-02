fafe028bc4fc81345d44de0844e0241887b768fa
Samir <Saitwalsamir@gmail.com>
2021-05-02 14:18:29 +0530

    --amazon_mobile app added
    --please create necessary API
    
    Co-Authored-By: damini321 <73902501+damini321@users.noreply.github.com>

--- Django_Amazon/amazon_admin/utils.py
+++ Django_Amazon/amazon_admin/utils.py
@@ -3,11 +3,11 @@ import uuid
 
 def Unique_Name():
     username = uuid.uuid4().hex[:8].upper()
-    print(username)
+    username = "{}{}".format("AD", username)
     return username
 
 
 def Unique_Password():
     password = uuid.uuid4().hex[:8].upper()
-    print(password)
+    password = "{}{}".format("AD", password)
     return password
