15c21ca27c2a0eda046d0516b556cdfa9b4d8ef6
Samir <Saitwalsamir@gmail.com>
2021-05-02 14:02:03 +0530

    --taks summery added
    Co-Authored-By: damini321 <73902501+damini321@users.noreply.github.com>

--- Django_Amazon/amazon_delivery_service/urls.py
+++ Django_Amazon/amazon_delivery_service/urls.py
@@ -13,4 +13,5 @@ urlpatterns = [
     path('manage_amazon_deactive_delivery_boy/', Manage_Amazon_Delivery_Boy_Deactive_List_View.as_view()),
     path('manage_amazon_delivery_boy/<int:id>/', Manage_Amazon_Delivery_Boy_Retrieve_View.as_view()),
     path('amazon_delivery_service_profile/', Amazon_Delivery_Service_Profile_View.as_view()),
+    # Create delivery boy profile
 ]
